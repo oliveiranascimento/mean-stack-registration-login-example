fb8b6a8c2cffc888209c56219c3a7deec63ad34e
oliveiranascimento <oliv.shell@gmail.com>
2019-09-13 21:09:16 -0300

    projeto mean stack

--- app/account/index.controller.js
+++ app/account/index.controller.js
@@ -41,6 +41,7 @@
                     FlashService.Error(error);
                 });
         }
+        
     }
 
 })();
---
+++ app/questions/index.controller.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Questions.IndexController', Controller);
+
+    function Controller($window, UserService, FlashService, QuestionService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.question = null;
+        vm.saveQuestion = saveQuestion;
+        vm.questionsList= [];
+
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+
+        function saveQuestion() {
+            let perguntaQueIraSerSalva = 
+            {
+                question: vm.question
+            };
+
+            QuestionService.Create(perguntaQueIraSerSalva)
+                .then(function () {
+                    FlashService.Success('Question Saved');
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                })
+            };
+
+            function GetAll() {
+                QuestionService.Create(perguntaQueIraSerSalva)
+                    .then(function () {
+                        FlashService.Success('Question Saved');
+                    })
+                    .catch(function (error) {
+                        FlashService.Error(error);
+                    })
+                };
+    }
+
+})();
